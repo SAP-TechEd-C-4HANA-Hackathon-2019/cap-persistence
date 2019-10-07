b10b82c2b752c7d172c0f6c33241d9f3d36e41c9
Fred Verheul <fred.verheul@gmail.com>
2019-10-07 16:49:01 +0200

    Added custom logic, error db lock

--- srv/store-service.js
+++ srv/store-service.js
@@ -1,12 +1,23 @@
-module.exports = srv => {
+//module.exports = srv => {
+module.exports = async function(srv) {
 
-    // if (srv.name === 'StoreService') {
+    const { Stores, VisitorCounts } = srv.entities
 
-    //     srv.after ('CREATE', 'VisitorCounts', vc => {
-    
-    //         let currentCount = vc
-            
-    
-    //     })
+    srv.after('CREATE', VisitorCounts, async function (vc) {
 
-  }
+        let visitorCount = vc.visitorCount
+        let paxdevice_ID = vc.paxdevice_ID;
+        console.log(visitorCount);
+
+        let store_id_array = await cds.run(SELECT.from(Stores).where({ paxdevice_ID: paxdevice_ID }));
+        console.log(store_id_array);
+        let store_id = store_id_array[0].ID;
+        console.log(store_id);
+        const ctx = cds.transaction(vc);
+        return await
+            ctx.run(
+                UPDATE(Stores).set({ currentVisitorCount: visitorCount }).where({ ID: store_id })
+            )
+    });
+
+}
